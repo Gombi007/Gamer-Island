faf34f7fc4e2cf574b396e2b9dd027f8348f12f0
Gyula Gombos <gombos88@gmail.com>
2022-06-06 17:08:33 +0200

    frontend header posotiton was fixed at top of the browser windows

--- Frontend/src/app/page-library/library/library.component.ts
+++ Frontend/src/app/page-library/library/library.component.ts
@@ -14,7 +14,7 @@ import { STRINGS } from 'src/app/strings.enum';
 export class LibraryComponent implements OnInit {
 
   innerHeight!: number;
-  headerHeight: number = 155;
+  headerHeight: number = 162;
   games: Game[] = [];
   gamesClone: Game[] = [];
   isPending: Boolean = false;
