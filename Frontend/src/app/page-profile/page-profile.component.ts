a59b68c26ece77f5f4b2828e683c9029756f2415
Gyula Gombos <gombos88@gmail.com>
2022-07-06 13:27:57 +0200

    frontend profile page form was added

--- Frontend/src/app/page-profile/page-profile.component.ts
+++ Frontend/src/app/page-profile/page-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { STRINGS } from '../strings.enum';
 
 @Component({
   selector: 'app-page-profile',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-profile.component.css']
 })
 export class PageProfileComponent implements OnInit {
+  innerHeight!: number;
+  headerHeight: number = STRINGS.HEADER_HEIGHT_FOR_CONTENT;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.innerHeight = window.innerHeight - this.headerHeight;
   }
+
+    // update value when resize
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+      this.innerHeight = window.innerHeight - this.headerHeight;
+    }
 
 }
