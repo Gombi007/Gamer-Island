0bef7d2b8cb7af8800d4db2b11cec2e1500d6bf6
Gyula Gombos <gombos88@gmail.com>
2022-07-06 16:07:56 +0200

    frontend profile get user data from server

--- Frontend/src/app/page-profile/page-profile.component.ts
+++ Frontend/src/app/page-profile/page-profile.component.ts
@@ -1,5 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { catchError, EMPTY, Subject, switchMap, tap } from 'rxjs';
+import { GlobalService } from '../global.service';
+import { AuthorizationService } from '../login/service/authorization.service';
 import { STRINGS } from '../strings.enum';
 
 @Component({
@@ -10,6 +15,9 @@ import { STRINGS } from '../strings.enum';
 export class PageProfileComponent implements OnInit {
   innerHeight!: number;
   headerHeight: number = STRINGS.HEADER_HEIGHT_FOR_CONTENT;
+  isPending = false;
+  user:{avatar:string,balance:number,email:string,userName:string,userUUID:string}=
+   {'avatar':'','balance':0.0,'email':'','userName':'','userUUID':''};
 
   userData = new FormGroup(
     {
@@ -19,11 +27,35 @@ export class PageProfileComponent implements OnInit {
     }
   );
 
-  constructor() { }
+  constructor(private http:HttpClient, private global:GlobalService, private route:Router, private author:AuthorizationService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  
     this.innerHeight = window.innerHeight - this.headerHeight;
+    this.getUserInformationByUUID$.subscribe();
+    //@ts-ignore
+    this.getUserInformationByUUID$.next();
   }
+
+  getUserInformationByUUID$ = new Subject().pipe(
+    tap(() => {
+      this.isPending = true;
+    }),
+    switchMap(() =>
+      this.http.get(STRINGS.API_USER_GET_USER_DATA + this.global.getUUIDFromLocalStore(), this.author.TokenForRequests())),
+    tap((data: any) => {
+      this.user = data;
+      console.log(this.user)
+   
+    }),
+    catchError(error => {
+      let message = error.error.error_message;
+      if (message.includes("Token has expired")) {
+        this.global.experiedSession = true;
+        this.route.navigate(['login']);
+      }
+      return EMPTY;
+    })
+  );
 
     // update value when resize
     @HostListener('window:resize', ['$event'])
@@ -33,6 +65,10 @@ export class PageProfileComponent implements OnInit {
 
     saveNewUserData(){
       console.log(this.userData.value);
+    }
+
+    balanceTopup(){
+
     }
 
 }
