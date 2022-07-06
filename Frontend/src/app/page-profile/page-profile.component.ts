c14bc42928058b5fd04ef8da7568837c90d5688d
Gyula Gombos <gombos88@gmail.com>
2022-07-06 17:47:39 +0200

    frontend  profile update methed was modified

--- Frontend/src/app/page-profile/page-profile.component.ts
+++ Frontend/src/app/page-profile/page-profile.component.ts
@@ -16,59 +16,65 @@ export class PageProfileComponent implements OnInit {
   innerHeight!: number;
   headerHeight: number = STRINGS.HEADER_HEIGHT_FOR_CONTENT;
   isPending = false;
-  user:{avatar:string,balance:number,email:string,userName:string,userUUID:string}=
-   {'avatar':'','balance':0.0,'email':'','userName':'','userUUID':''};
+  user: { avatar: string, balance: number, email: string, userName: string, userUUID: string } =
+    { 'avatar': '', 'balance': 0.0, 'email': '', 'userName': '', 'userUUID': '' };
 
-  userData = new FormGroup(
-    {
-      avatarURL: new FormControl("", Validators.required),
-      username: new FormControl("", Validators.required),
-      email: new FormControl("", Validators.required),
+    usernameInput = new FormControl({value:'username', disabled:true}, Validators.required);
+    avatarInput = new FormControl('avatarURL', Validators.required);
+    emailInput = new FormControl('email', Validators.required);
+  
+
+
+constructor(private http: HttpClient, private global: GlobalService, private route: Router, private author: AuthorizationService) { }
+
+ngOnInit(): void {
+  this.innerHeight = window.innerHeight - this.headerHeight;
+  this.getUserInformationByUUID$.subscribe();
+  //@ts-ignore
+  this.getUserInformationByUUID$.next();
+}
+
+getUserInformationByUUID$ = new Subject().pipe(
+  tap(() => {
+    this.isPending = true;
+  }),
+  switchMap(() =>
+    this.http.get(STRINGS.API_USER_GET_USER_DATA + this.global.getUUIDFromLocalStore(), this.author.TokenForRequests())),
+  tap((data: any) => {
+    this.user = data;
+    this.usernameInput.setValue(this.user.userName);
+    this.avatarInput.setValue(this.user.avatar);
+    this.emailInput.setValue(this.user.email);
+
+  }),
+  catchError(error => {
+    let message = error.error.error_message;
+    if (message.includes("Token has expired")) {
+      this.global.experiedSession = true;
+      this.route.navigate(['login']);
     }
-  );
+    return EMPTY;
+  })
+);
 
-  constructor(private http:HttpClient, private global:GlobalService, private route:Router, private author:AuthorizationService) { }
+// update value when resize
+@HostListener('window:resize', ['$event'])
+onResize() {
+  this.innerHeight = window.innerHeight - this.headerHeight;
+}
 
-  ngOnInit(): void {  
-    this.innerHeight = window.innerHeight - this.headerHeight;
-    this.getUserInformationByUUID$.subscribe();
-    //@ts-ignore
-    this.getUserInformationByUUID$.next();
+saveNewUserData(){
+  if(this.avatarInput.valid && this.emailInput.valid){
+    this.user.avatar = this.avatarInput.value;
+    this.user.email = this.emailInput.value;
+    console.log(this.user);
+  }else{
+    console.log("Fields are not valids");
   }
+}
 
-  getUserInformationByUUID$ = new Subject().pipe(
-    tap(() => {
-      this.isPending = true;
-    }),
-    switchMap(() =>
-      this.http.get(STRINGS.API_USER_GET_USER_DATA + this.global.getUUIDFromLocalStore(), this.author.TokenForRequests())),
-    tap((data: any) => {
-      this.user = data;
-      console.log(this.user)
-   
-    }),
-    catchError(error => {
-      let message = error.error.error_message;
-      if (message.includes("Token has expired")) {
-        this.global.experiedSession = true;
-        this.route.navigate(['login']);
-      }
-      return EMPTY;
-    })
-  );
+balanceTopup(){
 
-    // update value when resize
-    @HostListener('window:resize', ['$event'])
-    onResize() {
-      this.innerHeight = window.innerHeight - this.headerHeight;
-    }
-
-    saveNewUserData(){
-      console.log(this.userData.value);
-    }
-
-    balanceTopup(){
-
-    }
+}
 
 }
