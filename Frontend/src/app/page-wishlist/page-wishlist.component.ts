316db7ecc175ff5a86c4c8f196a04b19d95dd0c1
Gyula Gombos <gombos88@gmail.com>
2022-07-19 14:18:57 +0200

    frontend get user wishlist and show in a table feature was done

--- Frontend/src/app/page-wishlist/page-wishlist.component.ts
+++ Frontend/src/app/page-wishlist/page-wishlist.component.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, EMPTY, Subject, switchMap, tap } from 'rxjs';
+import { Game } from '../game.model';
+import { GlobalService } from '../global.service';
+import { AuthorizationService } from '../login/service/authorization.service';
 import { STRINGS } from '../strings.enum';
 
 @Component({
@@ -9,11 +15,16 @@ import { STRINGS } from '../strings.enum';
 export class PageWishlistComponent implements OnInit {
   innerHeight!: number;
   headerHeight: number = STRINGS.HEADER_HEIGHT_FOR_CONTENT;
+  isPending = false;
+  wishlist:Game[] =[];
 
-  constructor() { }
+  constructor(private http:HttpClient, private global:GlobalService, private router:Router,private author:AuthorizationService) { }
 
   ngOnInit(): void {
     this.innerHeight = window.innerHeight - this.headerHeight;
+    this.getUserWishlistGames$.subscribe();
+    //@ts-ignore
+    this.getUserWishlistGames$.next();
   }
 
     // update value when resize
@@ -21,5 +32,27 @@ export class PageWishlistComponent implements OnInit {
     onResize() {
       this.innerHeight = window.innerHeight - this.headerHeight;
     }
+
+    getUserWishlistGames$ = new Subject().pipe(
+      tap(() => {
+        this.isPending = true;
+      }),
+      switchMap(() =>
+        this.http.get(STRINGS.API_USER_GET_WISHLIST+this.global.getUUIDFromLocalStore(), this.author.TokenForRequests())),
+      tap((data: any) => {
+        this.isPending = false;
+        this.wishlist = data;
+      }),
+      catchError(error => {
+        let message = error.error.error_message;
+        if (message.includes("Token has expired")) {
+          this.global.experiedSession = true;
+          this.router.navigate(['login']);
+        }
+        this.isPending = false;
+        return EMPTY;
+      })
+    );
+  
 
 }
--- Frontend/src/app/strings.enum.ts
+++ Frontend/src/app/strings.enum.ts
@@ -8,6 +8,7 @@ export enum STRINGS {
     API_USER_GET_USER_DATA = 'http://localhost:8081/api/users/profile/',
     API_USER_UPDATE_USER_DATA = 'http://localhost:8081/api/users/profile/update',
     API_USER_UPDATE_USER_BALANCE = 'http://localhost:8081/api/users/profile/balance/',
+    API_USER_GET_WISHLIST = 'http://localhost:8081/api/users/wishlist/',
 
     API_ALL_GAMES_FOR_SHOP = 'http://localhost:8081/api/games/shop/',
     API_GAMES_DETAILS = 'http://localhost:8081/api/games/',
