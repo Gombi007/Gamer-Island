9c2d1739d3f1f863e278c9e6520ff0082d500ae2
Gyula Gombos <gombos88@gmail.com>
2022-07-19 14:00:24 +0200

    frontend wishlist page table was created

--- Frontend/src/app/page-wishlist/page-wishlist.component.ts
+++ Frontend/src/app/page-wishlist/page-wishlist.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { STRINGS } from '../strings.enum';
 
 @Component({
   selector: 'app-page-wishlist',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-wishlist.component.css']
 })
 export class PageWishlistComponent implements OnInit {
+  innerHeight!: number;
+  headerHeight: number = STRINGS.HEADER_HEIGHT_FOR_CONTENT;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.innerHeight = window.innerHeight - this.headerHeight;
   }
+
+    // update value when resize
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+      this.innerHeight = window.innerHeight - this.headerHeight;
+    }
 
 }
