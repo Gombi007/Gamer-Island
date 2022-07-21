75cc0c4381f6748d8a0070a2c623ee8324e00b1e
Gyula Gombos <gombos88@gmail.com>
2022-07-21 18:16:45 +0200

    wishlist get all genres subkect was created

--- Frontend/src/app/page-wishlist/page-wishlist.component.ts
+++ Frontend/src/app/page-wishlist/page-wishlist.component.ts
@@ -15,11 +15,11 @@ import { STRINGS } from '../strings.enum';
 })
 export class PageWishlistComponent implements OnInit {
   innerHeight!: number;
-  headerHeight: number = STRINGS.HEADER_HEIGHT_FOR_CONTENT+STRINGS.SEARCH_BAR__HEIGHT_FOR_CONTENT;
+  headerHeight: number = STRINGS.HEADER_HEIGHT_FOR_CONTENT + STRINGS.SEARCH_BAR__HEIGHT_FOR_CONTENT;
   isPending = false;
-  wishlist:Game[] =[];
-  wishlistGenres = ["Action","Strategy","Indie"];
-  defaultGenre ="All Genres"
+  wishlist: Game[] = [];
+  wishlistGenres = ["Action", "Strategy", "Indie"];
+  defaultGenre = "All Genres"
   search = new FormGroup(
     {
       name: new FormControl("", [Validators.minLength(3), Validators.maxLength(22)]),
@@ -27,56 +27,79 @@ export class PageWishlistComponent implements OnInit {
     }
   );
 
-  constructor(private http:HttpClient, private global:GlobalService, private router:Router,private author:AuthorizationService) { }
+  constructor(private http: HttpClient, private global: GlobalService, private router: Router, private author: AuthorizationService) { }
 
-  ngOnInit(): void {  
+  ngOnInit(): void {
     this.innerHeight = window.innerHeight - this.headerHeight;
     this.getUserWishlistGames$.subscribe();
     //@ts-ignore
     this.getUserWishlistGames$.next();
   }
 
-    // update value when resize
-    @HostListener('window:resize', ['$event'])
-    onResize() {
-      this.innerHeight = window.innerHeight - this.headerHeight;
-    }
+  // update value when resize
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerHeight = window.innerHeight - this.headerHeight;
+  }
 
-    getUserWishlistGames$ = new Subject().pipe(
-      tap(() => {
-        this.isPending = true;
-      }),
-      switchMap(() =>
-        this.http.get(STRINGS.API_USER_WISHLIST+this.global.getUUIDFromLocalStore(), this.author.TokenForRequests())),
-      tap((data: any) => {
-        this.isPending = false;
-        this.wishlist = data;
-      }),
-      catchError(error => {
-        let message = error.error.error_message;
-        if (message.includes("Token has expired")) {
-          this.global.experiedSession = true;
-          this.router.navigate(['login']);
-        }
-        this.isPending = false;
-        return EMPTY;
-      })
-    );
+  getUserWishlistGames$ = new Subject().pipe(
+    tap(() => {
+      this.isPending = true;
+    }),
+    switchMap(() =>
+      this.http.get(STRINGS.API_USER_WISHLIST + this.global.getUUIDFromLocalStore(), this.author.TokenForRequests())),
+    tap((data: any) => {
+      this.isPending = false;
+      this.wishlist = data;
+    }),
+    catchError(error => {
+      let message = error.error.error_message;
+      if (message.includes("Token has expired")) {
+        this.global.experiedSession = true;
+        this.router.navigate(['login']);
+      }
+      this.isPending = false;
+      return EMPTY;
+    })
+  );
+//TODO get all genre from user wishlist
+  getUserSearchedGames$ = new Subject().pipe(
+    tap(() => {
+      this.isPending = true;
+    }),
+    switchMap(() =>
+      this.http.get(STRINGS.API_USER_WISHLIST + this.global.getUUIDFromLocalStore(), this.author.TokenForRequests())),
+    tap((data: any) => {
+      this.isPending = false;
+      this.wishlist = data;
+    }),
+    catchError(error => {
+      let message = error.error.error_message;
+      if (message.includes("Token has expired")) {
+        this.global.experiedSession = true;
+        this.router.navigate(['login']);
+      }
+      this.isPending = false;
+      return EMPTY;
+    })
+  );
 
-      //convenience getter for easy access to form fields
+
+
+  //convenience getter for easy access to form fields
   get searchInputError(): { [key: string]: AbstractControl; } {
     return this.search.controls;
   }
 
-  
-    getSearchData() {
-      if (this.search.valid) {
-        let name = this.search.get('name')?.value;
-        let genre = this.search.get('genres')?.value;
-        let attributeAndValue = [name, genre];
-        console.log(attributeAndValue);       
 
-      }
+  getSearchData() {
+    if (this.search.valid) {
+      let name = this.search.get('name')?.value;
+      let genre = this.search.get('genres')?.value;
+      let attributeAndValue = [name, genre];
+      console.log(attributeAndValue);
+
     }
+  }
 
 }
